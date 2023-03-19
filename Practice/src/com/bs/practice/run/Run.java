19f05d85b411c138f96375d0dda9fd0062686b0c
Inho Choi <inhoru126@gmail.com>
2023-03-19 21:50:03 +0900

    학원과제

---
+++ Practice/src/com/bs/practice/run/Run.java
@@ -0,0 +1,38 @@
+package com.bs.practice.run;
+
+import com.bs.practice.func.OperatorPractice;
+import com.bs.practice.array.ArrayPractice;
+
+public class Run {
+	public static void main(String[] args) {
+		// new OperatorPractice().practice1();
+		// new OperatorPractice().practice2();
+		// new OperatorPractice().practice3();
+		// new OperatorPractice().practice4();
+		// new OperatorPractice().practice5();
+		// new OperatorPractice().practice6();
+		// new OperatorPractice().practice7();
+		// new OperatorPractice().practice8();
+		// new OperatorPractice().practice9();
+		// new OperatorPractice().practice10();
+		// new OperatorPractice().practice11();
+
+		// new ArrayPractice().practice1();
+		// new ArrayPractice().practice2();
+		// new ArrayPractice().practice3();
+		// new ArrayPractice().practice4();
+		// new ArrayPractice().practice5();
+		// new ArrayPractice().practice6();
+		// new ArrayPractice().practice7();
+		// new ArrayPractice().practice8();
+		// new ArrayPractice().practice9();
+		// new ArrayPractice().practice10();
+		// new ArrayPractice().practice11();
+		// new ArrayPractice().practice12();
+		// new ArrayPractice().practice13();
+		// new ArrayPractice().practice14();
+		// new ArrayPractice().practice15();
+		new ArrayPractice().practice16();
+	}
+
+}
